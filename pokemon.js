9e18a28ff730dc459de355e958218d1d52ba9835
Natassa <112552395+natko22@users.noreply.github.com>
2023-02-02 14:34:27 +0100

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,24 @@
+let count = 20
+
+
+function render(){
+  let container =  document.getElementById("pokemon")
+  container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg"/>`
+
+}
+
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+
+prev.onclick = function() {
+  if(count > 1){
+  count = count -1
+  render()
+}}
+next.onclick = function(){
+  if(count < 650){
+  count = count +1
+  render()
+}}
+
+render()
